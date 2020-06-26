54f51c0cf50cfce81d1b2d510b2f5eab801e81b8
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-26 20:54:26 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,7 +3,13 @@ CREATE TABLE users
     name TEXT,
     age INTEGER);
     
-CREATE TABLE pledges 
+CREATE TABLE projects 
+(id INTEGER PRIMARY KEY, 
+title TEXT, 
+category TEXT, 
+funding_goal FLOAT, 
+start_date TEXT, 
+end_date TEXT) 
 
 CREATE TABLE pledges 
     (id INTEGER PRIMARY KEY, 
